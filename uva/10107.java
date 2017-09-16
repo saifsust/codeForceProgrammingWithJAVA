f31297baf8beb312c4eef834cdbe597c18482091
Saiful Islam <saifsust@users.noreply.github.com>
2017-09-16 11:33:43 +0600

    first commit

---
+++ uva/10107.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+public class Main{
+	public static void main(String[] args)
+	{
+		List<Integer>list = new ArrayList<Integer>();
+		Scanner read = new Scanner(System.in);
+        while(read.hasNext())
+        {
+          int num = read.nextInt();
+          list.add(num);
+          Collections.sort(list);
+          if(list.size()%2==1)System.out.println(list.get(list.size()/2));
+          	else System.out.println((int)(list.get(list.size()/2)+list.get(list.size()/2-1) )/2);
+      
+        }
+		read.close();
+
+	}
+}
