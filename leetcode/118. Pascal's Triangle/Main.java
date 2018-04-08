94664d996a0a548a564982958192951df4129c88
Saiful Islam <saifsust@users.noreply.github.com>
2018-04-08 17:21:14 +0600

    119. Pascal's Triangle II

--- leetcode/118. Pascal's Triangle/Main.java
+++ leetcode/118. Pascal's Triangle/Main.java
@@ -3,62 +3,54 @@ import java.lang.*;
 import java.io.*;
 public class Main
 {
-
     public static void main(String[] args)
     {
-
         Solution sol = new Solution();
-        List<List<Integer>> tg = sol.generate(10);
-        for(int i = 0; i < tg.size(); i++)
-        {
-            for(int j = 0; j < tg.get(i).size(); j++)
-            {
-                System.out.print(tg.get(i).get(j) + "  ");
-            }
-            System.out.println();
-        }
+        List<Integer> list = sol.getRow(30);
+
+        for(int i = 0; i < list.size(); i++)
+            System.out.print(list.get(i) + "  ");
 
     }
 }
-
 class Solution
 {
-    public List<List<Integer>> generate(int numRows)
+    public List<Integer> getRow(int rowIndex)
     {
+        List<Integer> result = new ArrayList<Integer>();
+       
 
+        int[][] tri = new int[rowIndex + 2][10000];
 
-        List<List<Integer> > triangle = new ArrayList<>();
-        if(numRows == 0 )return triangle;
-        List<Integer> list = new ArrayList<Integer>();
-        list.add(1);
-        triangle.add(list);
+        tri[0][0] = 1;
 
-        for(int i = 1; i < numRows; i++)
+        for(int i = 1; i <= rowIndex; i++)
         {
-            for(int j = 0; j <= i + 1; j++)
+            for(int j = 0; j <= tri.length ; j++)
             {
-                list = new ArrayList<>();
-
-                for(int k = 0; k <= triangle.get(i - 1).size(); k++)
+                if(j == 0)
                 {
-                    if(k == 0)
-                    {
-                        list.add(triangle.get(i - 1).get(k));
-                        continue;
-                    }
-                    if(k == triangle.get(i - 1).size())
-                    {
-                        list.add(triangle.get(i - 1).get(k - 1));
-                        continue;
-                    }
-
-                    list.add(triangle.get(i - 1).get(k) + triangle.get(i - 1).get(k - 1));
+                    tri[i][j] = tri[i - 1][0];
+                    continue;
                 }
+                tri[i][j] = tri[i - 1][j] + tri[i - 1][j - 1];
 
             }
-            triangle.add(list);
+
         }
 
-        return triangle;
+        /*    for(int i = 0; i <= rowIndex; i++)
+            {
+                for(int j = 0; j < tri.length && tri[i][j] !=0; j++)
+                {
+                    System.out.print(tri[i][j] + "  ");
+                }
+                System.out.println();
+            }
+        */
+        for(int i = 0; i < tri[rowIndex].length && tri[rowIndex][i] != 0 ; i++)result.add(tri[rowIndex][i]);
+
+
+        return result;
     }
 }
