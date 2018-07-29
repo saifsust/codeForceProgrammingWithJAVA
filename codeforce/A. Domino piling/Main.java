fee34b843a06f6bc670413c1cda47d2d92e92657
Saiful Islam <saifsust@users.noreply.github.com>
2018-07-29 20:19:20 +0600

    https://codeforces.com/problemset/problem/50/A

---
+++ codeforce/A. Domino piling/Main.java
@@ -0,0 +1,94 @@
+import java.util.*;
+import java.io.*;
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+
+        Reader read = new Reader();
+
+        int m, n;
+        final int d = 2;
+        while((m = read.nextInt()) != -1)
+        {
+            n = read.nextInt();
+            int square = m * n;
+            System.out.println((int)Math.floor(square / d));
+
+
+        }
+
+    }
+
+
+
+
+    /// faster input reader
+    private static class Reader
+    {
+        private BufferedReader buffer;
+        private StringTokenizer tokenizer;
+
+        public Reader()
+        {
+            this.buffer = new BufferedReader(new InputStreamReader(System.in), 31768);
+            this.tokenizer = null;
+        }
+
+        public String next()
+        {
+            while(tokenizer == null || !tokenizer.hasMoreTokens())
+            {
+                try
+                {
+                    tokenizer = new StringTokenizer(buffer.readLine());
+                }
+                catch(Exception ex)
+                {
+                    return "";
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt()
+        {
+            try
+            {
+                return Integer.parseInt(next());
+            }
+            catch(Exception ex)
+            {
+                return -1;
+            }
+        }
+
+        public long nextLong()
+        {
+            try
+            {
+                return Long.parseLong(next());
+            }
+            catch(Exception ex)
+            {
+                return -1;
+            }
+
+        }
+        public double nextDouble()
+        {
+            try
+            {
+                return Double.parseDouble(next());
+            }
+            catch(Exception ex)
+            {
+                return -1.0;
+            }
+
+        }
+
+
+    }
+}
