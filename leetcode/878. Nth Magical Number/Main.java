74aa97149e9c0efb9097ac102ab948c5228c2b23
Saiful Islam <saifsust@users.noreply.github.com>
2018-07-30 12:26:39 +0600

    878. Nth Magical Number final Math solution is Important
    formula == n th num will be floor( nth number value /a)+ floor( nth number value /b)- floor( nth number value /lcm)
    here a,b are given number

--- leetcode/878. Nth Magical Number/Main.java
+++ leetcode/878. Nth Magical Number/Main.java
@@ -1,78 +1,137 @@
+/**
+ *@NAME SAIFUL ISLAM
+ *@BATCH SUST CSE 2013-14
+ *@Email : saiful.sust.cse@gmail.com
+ *@Facebok : https://www.facebook.com/SaifulIslamLitonSust
+*/
+
+
 import java.util.*;
 import java.io.*;
 public class Main
 {
+
     public static void main(String[] args)
     {
-        Scanner  read = new Scanner(System.in);
-        while(read.hasNext())
+
+        //Scanner read = FileFactory.read();
+        Reader read = new Reader();
+        int n, a, b;
+        while((n = read.nextInt()) != -1)
         {
-            System.out.println(new Solution().nthMagicalNumber(read.nextInt(), read.nextInt(), read.nextInt()));
+            a = read.nextInt();
+            b = read.nextInt();
+            System.out.println(new Solution().nthMagicalNumber(n, a, b));
         }
+
+    }
+
+
+
+    /// faster input reader
+    private static class Reader
+    {
+        private BufferedReader buffer;
+        private StringTokenizer tokenizer;
+
+        public Reader()
+        {
+            this.buffer = new BufferedReader(new InputStreamReader(System.in), 31768);
+            this.tokenizer = null;
+        }
+
+        public String next()
+        {
+            while(tokenizer == null || !tokenizer.hasMoreTokens())
+            {
+                try
+                {
+                    tokenizer = new StringTokenizer(buffer.readLine());
+                }
+                catch(Exception ex)
+                {
+                    return "";
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt()
+        {
+            try
+            {
+                return Integer.parseInt(next());
+            }
+            catch(Exception ex)
+            {
+                return -1;
+            }
+        }
+
+        public long nextLong()
+        {
+            try
+            {
+                return Long.parseLong(next());
+            }
+            catch(Exception ex)
+            {
+                return -1;
+            }
+
+        }
+        public double nextDouble()
+        {
+            try
+            {
+                return Double.parseDouble(next());
+            }
+            catch(Exception ex)
+            {
+                return -1.0;
+            }
+
+        }
+
+
     }
 }
+
+
+
 class Solution
 {
-
     public int nthMagicalNumber(int N, int A, int B)
     {
+        long lcm = (long) ((A * B) / gcd(A, B));
+        long high = (long) Math.max(A, B) * N;
+        //System.out.println(high);
+        long mod = (long)1e9 + 7;
+        long magicNum = binarySearch(0, high, (long)N, (long)A, (long)B, lcm);
+        return (int) (magicNum % mod);
 
-        long  lcm = (long) A / gcd(A, B) * B;
-        // System.out.println(lcm);
-        long max = (long) Math.max(A, B);
-        long high = (long) 1e15; //(long) max * (N + 4);
-        long mod = (long) 1e9 + 7;
-        //System.out.println(mod);
-        long  magicNum =  binarySearch(0, high, (long) N, lcm, (long)A, (long)B) % mod;
-        /*while(magicNum % A != 0 && magicNum % B != 0)
-        {
-            --magicNum;
-        }*/
-        //if(magicNum == 999720000)return(int) (magicNum + 7);
-        /* long low = 0;
-         while(low < high)
-         {
-             long mid = low + (high - low) / 2;
-            // System.out.println(mid);
-             long numOfMagicNum = (long)Math.floor(mid / A) + (long)Math.floor(mid / B) - (long)Math.floor(mid / lcm);
-             if(numOfMagicNum < N) low = mid + 1;
-             else high = mid;
-
-         }*/
-        //System.out.println(low);
-
-        return(int)(magicNum % mod); //(int)(low % mod);
     }
 
-    private long binarySearch(long low, long high, long searchKey, long lcm, long a, long b)
+    private long binarySearch(long low, long high, long searchKey, long a, long b, long lcm)
     {
-
         if(low < high)
         {
+            long mid = (long)( (high + low) / 2);
 
-            long mid = (long) (low + ( high - low) / 2);
-            /**
-             *  floor(x/a)+floor(x/b) -floor(x/lcm)
-             * here x is the magic number such as
-             *  for 2 ,3
-             *   floor(12/2)=6
-             *   floor(12/3)=4
-             *   floor(12/6)=2
-             *  serial of magic number = 6+4-2 = 8
-             *  this is the mathematical logic and equation
-             * numofMagicNum is equal or less than N
-             */
-            long numOfMagicNum = (long) (Math.floor(mid / a) + Math.floor(mid / b) - Math.floor(mid / lcm));
-            if(numOfMagicNum < searchKey)
-                return binarySearch(mid + 1, high, searchKey, lcm, a, b);
-            else return binarySearch(low, mid, searchKey, lcm, a, b);
+            long nthNum = (long) (Math.floor(mid / a) + Math.floor(mid / b) - Math.floor(mid / lcm));
+            if(nthNum == searchKey) return Math.max( a * (mid / a), b * (mid / b));
+            else if(nthNum < searchKey) return binarySearch(mid + 1, high, searchKey, a, b, lcm);
+            else return binarySearch(low, mid, searchKey, a, b, lcm);
         }
+        // System.out.println(low + "  " + high);
         return low;
-    }
 
+    }
     private int gcd(int m, int n)
     {
-        if(n == 0) return m;
-        return gcd(n, m % n);
+        if(m == 0)return n;
+        return gcd(n % m, m);
     }
+
+
 }
