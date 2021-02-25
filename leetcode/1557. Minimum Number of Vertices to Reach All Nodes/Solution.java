45459188cdf7aac265447d6b1ab378bb37869dec
Saiful Islam <saifsust@users.noreply.github.com>
2021-02-25 14:18:31 +0600

    Simple O(n)

--- leetcode/1557. Minimum Number of Vertices to Reach All Nodes/Solution.java
+++ leetcode/1557. Minimum Number of Vertices to Reach All Nodes/Solution.java
@@ -1,32 +1,12 @@
 class Solution {
     public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
-        List<Integer> minVertex = new ArrayList<Integer>();
-        Integer len = Integer.MAX_VALUE;
-        for(int source = 0; source<n; source++){
-           boolean visited[] = new boolean[n+1];
-            List<Integer> entry = new ArrayList<Integer>();
-            for(int start = source; start< n; start++){
-                if(visited[start]) continue;
-                 entry.add(start);
-                 dfs(start, visited, edges);
-            }
-            if(len > entry.size()){
-               len = entry.size();
-                minVertex.clear();
-                minVertex.addAll(entry);
-            }
+        int checker[] = new int[n];
+        for(List<Integer> edge : edges){
+            checker[edge.get(1)]--;
         }
-        
-        return minVertex;
+        List<Integer> vertices = new ArrayList<Integer>();
+        for(int i = 0; i<n; i++) if(checker[i] == 0) vertices.add(i);
+        return vertices;
     }
-    private void dfs(int source, boolean visited[], List<List<Integer>> edges){
-         visited[source] = true;
-         for(List<Integer> next : edges){
-             if(next.get(0) == source){
-                 if(visited[next.get(1)]) continue;
-                 dfs(next.get(0), visited, edges);
-             }
-             
-         }
-    }
+    
 }
