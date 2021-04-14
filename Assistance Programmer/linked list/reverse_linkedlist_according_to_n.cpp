f015c41eb829741a16bab02f389161fd12046efa
Saiful Islam <saifsust@users.noreply.github.com>
2021-04-14 11:14:43 +0600

    reverse linked list according to n

--- Assistance Programmer/linked list/reverse_linkedlist_according_to_n.cpp
+++ Assistance Programmer/linked list/reverse_linkedlist_according_to_n.cpp
@@ -17,3 +17,27 @@ struct Node* reverse_according_to_n(struct Node *head, int n){
 	temp->next = prev_from_tail;
 	return prev_from_head;
 }
+
+/**
+ * better than first solution
+**/
+
+
+struct Node* reverse_according_to_n(struct Node *head, int n){
+	struct Node *prev_from_head=NULL, *prev_from_tail=NULL, *next=NULL, *tail=NULL;
+	while(head){
+		next = head->next;
+		if(n>0){
+			head->next= prev_from_head;
+			if(prev_from_head == NULL) tail = head;
+			prev_from_head = head;
+		}else{
+			head->next = prev_from_tail;
+			prev_from_tail = head;
+		}
+        n--;
+		head = next;
+	}
+	tail->next = prev_from_tail;
+	return prev_from_head;
+}
