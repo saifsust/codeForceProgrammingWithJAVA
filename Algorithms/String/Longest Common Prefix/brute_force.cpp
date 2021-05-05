47352d971ad88139c0fcae1b0083222686fada0f
Saiful Islam <saifsust@users.noreply.github.com>
2021-05-05 13:38:24 +0600

    brute force for longest common prefix

--- Algorithms/String/Longest Common Prefix/brute_force.cpp
+++ Algorithms/String/Longest Common Prefix/brute_force.cpp
@@ -1,28 +1,15 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-// longest common Prefix
-
-
-int main() {
-
-	const int N = 100;
-	string arr[N];
-	int n;
-	while(cin >> n) {
-		for(int i = 0; i<n; i++) cin >> arr[i];
-		string prefix= arr[0];
-		for(int count = 1; count < n; count++) {
+  string longestCommonPrefix (string arr[], int N)
+    {
+        string prefix= arr[0];
+		for(int count = 1; count < N; count++) {
 		//	cout << prefix << endl;
 			string temp = "";
 			for(int i = 0; i<prefix.size() && i<arr[count].size(); i++) {
 				if(prefix[i] != arr[count][i]) break;
 				temp += prefix[i];
 			}
-			temp+='\0';
+			if(temp.compare("")) temp+='\0';
 			prefix= temp;
 		}
-		cout << prefix << endl;
-	}
-	return 0;
-}
+		return prefix == "" ? "-1" : prefix;
+    }
