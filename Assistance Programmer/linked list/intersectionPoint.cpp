694ad370bb41218f9fbf547eb7c43cdfc3558127
Saiful Islam <saifsust@users.noreply.github.com>
2021-05-08 16:42:29 +0600

    Intersect Point using two pointer algorithm

--- Assistance Programmer/linked list/intersectionPoint.cpp
+++ Assistance Programmer/linked list/intersectionPoint.cpp
@@ -1,19 +1,15 @@
-//Function to find intersection point in Y shaped Linked Lists.
 int intersectPoint(Node* head1, Node* head2)
 {
-      map<Node*, bool> mapper;
-      while(head1 || head2){
-      	if(head1){
-      		if(mapper[head1]) return head1->data;
-      		mapper[head1] = true;
-      		head1 = head1->next;
-		  }
-		 if(head2){
-		 	if(mapper[head2]) return head2->data;
-		 	mapper[head2] = true;
-		 	head2 = head2->next;
-		 } 
+      struct Node *root1 = head1, *root2 =  head2;
+      while(root1 != root2){
+      	 root1 = root1->next;
+      	 root2 = root2->next;
+      	 if(root1 == NULL){
+      	 	root1 = head2;
+		   }
+		 if(root2 == NULL){
+		 	root2 = head1;
+		 }
 	  }
-         
-      return -1;
+      return root1 == NULL ? -1 : root1 -> data;
 }
