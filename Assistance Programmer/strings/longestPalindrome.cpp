b2ac2ed4cb7ca8608f0daeaa7f05a158d48e848b
Saiful Islam <saifsust@users.noreply.github.com>
2021-05-12 20:40:30 +0600

    Longest Palindrome using recursion

--- Assistance Programmer/strings/longestPalindrome.cpp
+++ Assistance Programmer/strings/longestPalindrome.cpp
@@ -1,10 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
 
-// Longest Palindrome using recursion 
 string palindrome(string s, int start, int end){
      static string store = "";
-     if(end - start + 1  < store.size()) return store;
+     if(end - start + 1  <= store.size()) return store;
      if(start == end) return to_string(s[start]);
-     //cout << s.substr(start, end - start +1) << endl;
+    // cout << s.substr(start, end - start +1) << endl;
      bool isPalindrome = true;
      for(int i = start, j=end; i<j;i++, j--){
          if(s[i] != s[j]){
@@ -16,8 +17,21 @@ string palindrome(string s, int start, int end){
          store = s.substr(start, end-start + 1);
          return store;
      } 
-     string left = palindrome(s, start, end - 1);
+     string left  = palindrome(s, start, end - 1);
      string right = palindrome(s, start + 1 , end);
      if(left.size() > right.size()) return left;
      return right;
+}
+string longestPalindrome(string s){
+    return palindrome(s, 0, s.size()-1);
+}
+
+
+int main(){
+    freopen("in.in", "r", stdin);
+    freopen("out.in", "w", stdout);
+    string str;
+    cin >> str;
+    cout << longestPalindrome(str) << endl;
+    return 0;
 }
