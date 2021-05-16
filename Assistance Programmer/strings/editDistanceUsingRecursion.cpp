074518d394eb620a01dcd7494c22968255f28bf8
Saiful Islam <saifsust@users.noreply.github.com>
2021-05-16 16:02:58 +0600

    Update editDistanceUsingRecursion.cpp

--- Assistance Programmer/strings/editDistanceUsingRecursion.cpp
+++ Assistance Programmer/strings/editDistanceUsingRecursion.cpp
@@ -13,6 +13,24 @@ int64_t editDistance(string str1, string str2, int first, int second){
     int64_t remove = 1 + editDistance(str1, str2 , first, second + 1);
     return min(insert, min(replace, remove));
 }
+// edit distance using dynamic and recursive programming
+
+int64_t editDist(string str, string str2, int first, int second, vector<vector<int>> &dp){
+     if(first >= str.size()) return dp[first][second] = str2.size() - second;
+     if(second >= str2.size()) return dp[first][second] = str.size() - first;
+     if(dp[first][second] != -1) return dp[first][second];
+     if(str[first] == str2[second]){
+         if(dp[first][second] != -1) return dp[first][second];
+         else return dp[first][second] = editDist(str, str2, first + 1, second + 1, dp);
+     }
+
+     int64_t rp = editDist(str, str2, first + 1, second + 1, dp);
+     int64_t rm = editDist(str, str2, first, second + 1, dp);
+     int64_t in = editDist(str, str2, first + 1, second, dp);
+    return dp[first][second] = 1 + min(rp, min(rm, in));
+    }
+
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
