3cda2d6cec73dfdfa2068418a017286dad406b1b
Saiful Islam <saifsust@users.noreply.github.com>
2023-10-24 12:33:52 +0600

    O(n^2) solution

--- GeeksForGeek/string/ongest-k-unique-characters-substring/TL.cpp
+++ GeeksForGeek/string/ongest-k-unique-characters-substring/TL.cpp
@@ -1,46 +1,27 @@
-int longestKSubstr(string s, int k) {
-     // Naive solution O(n ^ 2)
-     
-     
-     int ans  = -1;
-     if(k == 0) return ans;
-     
-     for(int i = 0; i < s.size(); i++){
-          
-          int ck = k;
-          int arr[27];
-          
-          memset(arr, 0, sizeof(arr));
-          
-          
-          for(int j = i; j<=s.size(); j++){
-              
-              if(j == s.size()){
-                  if(ck == 0) ans = max(ans, j - i);
-                  break;
-              }
-              
-              int idx = s[j] - 'a';
-              
-              
-              if(arr[idx] == 0 && ck == 0){
-                  ans = max(ans, j - i);
-                  break;
-              }
-              
-              if(arr[idx] == 0) --ck;
-              
-              
-              ++arr[idx];
-              
-          }
-          
-          
-          
-          
-     }
-     
-     
-     return ans;
-     
+ int longestKSubstr(string s, int k) {
+        
+        int ans = -1;
+        
+        if(!k) return ans; 
+        
+        
+        for(int i = 0; i< s.size(); i++){
+            int arr[26], ck= k;
+            memset(arr, 0 , sizeof(arr));
+            
+            for(int j = i; j<=s.size(); j++){
+                
+                if((j == s.size() || arr[s[j] - 'a'] == 0) && ck == 0){
+                    ans = max(ans, j - i);
+                    break;
+                }
+                
+                if(arr[s[j] - 'a'] == 0 ) --ck;
+                
+                ++arr[s[j] - 'a'];
+                
+            }
+        }
+        
+        return ans;
     }
