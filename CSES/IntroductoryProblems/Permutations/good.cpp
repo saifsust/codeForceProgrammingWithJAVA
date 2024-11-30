69819526605710a8c0d15806583bdd85d0865067
Saiful Islam <saifsust@users.noreply.github.com>
2024-11-30 11:24:59 +0600

    adhoc

---
+++ CSES/IntroductoryProblems/Permutations/good.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  while(cin >> n){
+
+    if(n == 1){
+        cout << 1 ;
+        continue;
+    }
+    if( 2<= n && n <= 3){
+        cout << "NO SOLUTION"<<endl;
+        continue;
+    }
+
+    for(int i = 2; i<=n; i+=2){
+        cout << i << " ";
+    }
+
+    for(int i = 1; i<=n; i+=2){
+        cout << i;
+        if(i < n -1){
+            cout << " ";
+        }
+    }
+
+    cout << endl;
+
+
+  }
+  return 0;
+}
